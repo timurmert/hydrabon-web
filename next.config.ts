6da098dd54939c91cc4f713ce50a42f7916d9ff0
Timur Mert USTA <timurmertusta@gmail.com>
2025-10-29 22:53:09 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 480, 640, 768, 1024, 1280, 1536, 1920],
     imageSizes: [320, 480, 640, 768],
+    qualities: [75, 90, 100],
+  },
+  turbopack: {
+    root: process.cwd(),
   },
 };
 
